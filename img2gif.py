d2940e1f6c8ba21d68d98a0cb778b013a82838a7
Deric <82902621+dericmg@users.noreply.github.com>
2025-12-30 13:21:27 -0300

    Add files via upload

---
+++ img2gif.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tempo", type=int, default=100)
+args = parser.parse_args()
+
+images = []
+for img in sorted(os.listdir(), key=lambda x: (int(x.split('.')[0]) if x.split('.')[0].isdigit() else float('inf'))):
+    if img.lower().endswith(('.jpeg', '.jpg', '.png', '.bmp')):
+        images.append(Image.open(img).convert("P", palette=Image.ADAPTIVE))
+
+if images:
+    images[0].save(
+        "output.gif",
+        save_all=True,
+        append_images=images[1:],
+        duration=args.tempo,
+        loop=0
+    )
